fdde2a4dc8f23afd213beaf34ad10f458e3c270f
soha-zamani <sohaaa.zamani@gmail.com>
2026-02-26 12:15:50 +0100

    mart

---
+++ models/mart/mart_weather_weekly.sql
@@ -0,0 +1,3 @@
+'''In a table `mart_weather_weekly.sql` we want to see **all** weather stats from the `prep_weather_daily` model aggregated weekly. 
+
+- consider whether the metric should be Average, Maximum, Minimum, Sum or [Mode](https://wiki.postgresql.org/wiki/Aggregate_Mode)
